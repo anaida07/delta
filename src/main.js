10c7179ae451f37f78bb2dd8546315293468c336
anaida07 <get.aneeta@gmail.com>
2019-08-27 12:38:20 +0545

    Working version of electron-forge with squirrel build

--- src/main.js
+++ src/main.js
@@ -1,20 +1,121 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, autoUpdater} = require('electron')
+if (require('electron-squirrel-startup')) return;
+
+const {app, BrowserWindow, autoUpdater, dialog} = require('electron')
+
+// this should be placed at top of main.js to handle setup events quickly
+if (handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
+
 const path = require('path')
+const log = require('electron-log');
+// const {autoUpdater} = require("electron-updater");
+
+// autoUpdater.logger = log;
+// autoUpdater.logger.transports.file.level = 'info';
+// log.info('App starting...');
 
 const DOMAIN = 'http://localhost:9999';
 const suffix = process.platform === 'darwin' ? `/RELEASES.json?method=JSON&version=${app.getVersion()}` : '';
 autoUpdater.setFeedURL({
-  url: `${DOMAIN}/proton/a8d003cbc2a2ba91b90adba07c1fb2c9/${process.platform}/${process.arch}${suffix}`,
+  url: `${DOMAIN}/Donut/6cc5674f8826743540f152dbfbb014d5/${process.platform}/${process.arch}${suffix}`,
   serverType: 'json',
 });
 setInterval(() => {
   autoUpdater.checkForUpdates()
 }, 30000)
 
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts, (response) => {
+    if (response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', message => {
+  console.error('There was a problem updating the application')
+  console.error(message)
+})
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  mainWindow.webContents.send('message', text);
+}
 
 function createWindow () {
   // Create the browser window.
@@ -41,10 +142,43 @@ function createWindow () {
   })
 }
 
+// autoUpdater.on('checking-for-update', () => {
+//   sendStatusToWindow('Checking for update...');
+// })
+// autoUpdater.on('update-available', (ev, info) => {
+//   sendStatusToWindow('Update available.');
+// })
+// autoUpdater.on('update-not-available', (ev, info) => {
+//   sendStatusToWindow('Update not available.');
+// })
+// autoUpdater.on('error', (ev, err) => {
+//   sendStatusToWindow('Error in auto-updater.');
+// })
+// autoUpdater.on('download-progress', (ev, progressObj) => {
+//   sendStatusToWindow('Download progress...');
+// })
+// // autoUpdater.on('update-downloaded', (ev, info) => {
+// //   sendStatusToWindow('Update downloaded; will install in 5 seconds');
+// // });
+
+// autoUpdater.on('update-downloaded', (ev, info) => {
+//   // Wait 5 seconds, then quit and install
+//   // In your application, you don't need to wait 5 seconds.
+//   // You could call autoUpdater.quitAndInstall(); immediately
+//   setTimeout(function() {
+//     autoUpdater.quitAndInstall();  
+//   }, 5000)
+// })
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdates();
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
